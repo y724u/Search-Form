24678ce302aade43afa4f4da5a06321b496525fc
y724u <68385107+y724u@users.noreply.github.com>
2020-11-19 14:21:23 +0900

    検索ボックスでアラートを表示

--- script.js
+++ script.js
@@ -1,14 +1,74 @@
-$(function() {
-    // <!-- ダウンロードボタン活性化&非活性化 -->
-    $('.js-checkbox').on('change', function () {
-      const target = $(this).data('target');
-      const language = document.getElementById(target);
-      if ($(this).prop('checked')){
-        $(language).addClass('box__checked');
-      }else{
-        $(language).removeClass('box__checked');
-      }
-    });
+$(function () {
+  // <!-- プログラミング言語ボタン活性化&非活性化 -->
+  $('.js-checkbox').on('change', function () {
+    const target = $(this).data('target');
+    const language = document.getElementById(target);
+    if ($(this).prop('checked')) {
+      $(language).addClass('box__checked');
+    } else {
+      $(language).removeClass('box__checked');
+    }
+  });
+
+  $('form').submit(function () {
+    if ($('li').hasClass('box__checked')) {
+      const selectLanguage = $('.box__checked').text();
+      alert('「検索対象は、' + selectLanguage + 'です');
+    } else {
+      alert('選択されていません');
+    }
+  });
+
+  // $('form').submit(function () {
+  //   if ($('li').hasClass('box__checked')) {
+  //     const [] = $('.js-checkbox').data('target');
+  //     console.log([]);
+  //     alert('「検索対象は、' + selectLanguage + 'です');
+  //   } else {
+  //     alert('選択されていません');
+  //   }
+  // });
+
+
+
+  // $('form').submit(function () {
+  //   if ($('li').hasClass('box__checked')) {
+  //     const selectLanguage = $('.box__checked').val();
+  //     alert('「検索対象は、' + selectLanguage + 'です');
+  //   } else {
+  //     alert('選択されていません');
+  //   }
+  // });
+
+
+  // $('form').submit(function () {
+  //   if ($(".js-checkbox").prop('checked')) {
+  //     const selectLanguage = $('.js-checkbox').text();
+  //     alert('「検索対象は、' + selectLanguage + 'です');
+  //   } else {
+  //     alert('選択されていません');
+  //   }
+  // });
+
+//   $('form').submit(function () {
+//   $('.js-checkbox').on('change',function() {
+//     if
+//     const result = $('checkbox:checked').text();
+//     alert( result );
+// })
+// });
+
+
+  // const search = $('input').hasClass('box__checked');
+  // console.log(search);
+
+  //    $('form').submit(function() {
+
+  //     alert('「検索内容は、' +  '」');
+
+  // });
+
+
 });
 
 

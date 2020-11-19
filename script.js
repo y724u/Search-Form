77ed900d630d21bd8a1611880b402a4dbc9ebed9
y724u <68385107+y724u@users.noreply.github.com>
2020-11-19 12:08:09 +0900

    js操作　boxの背景色を変更する

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 $(function() {
-  
-
+    // <!-- ダウンロードボタン活性化&非活性化 -->
+    $('.js-checkbox').on('change', function () {
+      const target = $(this).data('target');
+      const language = document.getElementById(target);
+      if ($(this).prop('checked')){
+        $(language).addClass('box__checked');
+      }else{
+        $(language).removeClass('box__checked');
+      }
+    });
 });
+
+
+
+
+
+
+
+//  // // <!-- サービスシステム開発モーダル -->
+//  let scrollPosition1;
+//  $('.js-modalOpen').on('click', function () {
+//    const target = $(this).data('target');
+//    const modal = document.getElementById(target);
+//    scrollPosition1 = $(window).scrollTop();
+//    $(modal).addClass('show_modal');
+//    $('body').addClass('fixed').css({ 'top': -scrollPosition1 });
+//    return false;
+//  });
+//  $('.js-modalClose').on('click', function () {
+//    $('.js--modal').removeClass('show_modal');
+//    $('body').removeClass('fixed').css({ 'top': 0 });
+//    window.scrollTo(0, scrollPosition1);
+//  });

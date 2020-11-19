bc6ef8f1569dd2ddf5a0b9664b74047694bb4e0c
y724u <68385107+y724u@users.noreply.github.com>
2020-11-19 22:59:48 +0900

    全体を調整

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(function () {
   const selectLanguage = [];
-  $('form').submit(function () {
+  $('.js-form').on('submit',function () {
     selectLanguage.length = 0;
     $('.js-checkbox:checked').each(function () {
       selectLanguage.push($(this).val());
     });
     alert('「検索対象は、' + selectLanguage + 'です。」');
+    event.preventDefault();
   });
 });

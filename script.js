5a3272c7b60b53ef5a560422ea619944a8bd0732
y724u <68385107+y724u@users.noreply.github.com>
2020-11-20 22:02:47 +0900

    jsを修正

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 $(function () {
   const selectLanguage = [];
-  $('.js-form').on('submit', function () {
-    selectLanguage.length = 0;
-    $('.js-checkbox:checked').each(function () {
-      selectLanguage.push($(this).val());
-    });
-    const answer = selectLanguage.join('、');
-    alert('「検索対象は、' + answer + 'です。」');
+  $('.js-form').on('submit', function (event) {
     event.preventDefault();
+    selectLanguage.length = 0;
+    $('.js-checkbox:checked').each(function (index, el) {
+      selectLanguage.push($(el).val());
+    });
+    alert(`「検索対象は、${selectLanguage.join('、')}です。`);
   });
 });
